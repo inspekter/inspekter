80542420e5ef9e9f1a0dd4f2264a2df16793c95e
Pablo Oliveira <kryptogeist@gmail.com>
2017-12-04 21:33:23 +0000

    add plugin and package laoders

---
+++ src/cli/index.js
@@ -0,0 +1,46 @@
+'use strict'
+
+const program = require('commander')
+const inspekter = require('../index.js')
+const output = require('./output.js')
+
+const VERSION = require('../../package.json').version
+
+program
+  .version(VERSION)
+  .usage('[options] <file>')
+  .option('-d --dest <dest>', 'Destination folder for the report output')
+  .option('-i --ignore <ignore>', 'Pattern to ignore')
+  .option('-o --output <format>', 'Output format json|html|console')
+  .parse(process.argv)
+
+function writeOutput(report, format) {
+  switch (format) {
+    case output.FORMATS.JSON:
+      output.json(report, 'report.json')
+      break;
+
+    case output.FORMATS.HTML:
+      // TODO
+      break;
+
+    default:
+      output.console(report)
+  }
+}
+
+if (program.args.length === 0) {
+  console.error('ERROR: You must provide at least one file.')
+} else {
+  let options = {
+    ignore: program.ignore || null
+  }
+
+  inspekter.analyse(program.args, options)
+    .then((report) => {
+      writeOutput(report, options.format, options.dest)
+    })
+    .catch((error) => {
+      console.error(error)
+    })
+}
---
+++ src/cli/output.js
@@ -0,0 +1,60 @@
+'use strict'
+
+const chalk = require('chalk')
+const fs = require('fs')
+const Table = require('cli-table')
+
+module.exports.FORMATS = {
+  CONSOLE: 'console',
+  JSON: 'json',
+  HTML: 'html'
+}
+
+module.exports.console = (report) => {
+  let table
+
+  // TODO: choose color based on result
+  report.reports.forEach((item) => {
+    table = new Table();
+
+    table.push(
+
+      // File
+      { 'File': `${chalk.green(item.file.name)}`},
+      { 'Path': `${chalk.green(item.file.path)}`},
+
+      // Maintainability
+      { 'Maintainability': `${chalk.blue(item.maintainability)}`},
+
+      // Lines of code
+      { 'Logical lines of code': `${chalk.blue(item.sloc.logical)}`},
+      { 'Physical lines of code': `${chalk.blue(item.sloc.physical)}`},
+
+      // Cyclomatic complexity
+      { 'Cyclomatic complexity': `${chalk.blue(item.cyclomatic)}`},
+
+      // Cyclomatic Density
+      { 'Cyclomatic density': `${chalk.blue(item.cyclomaticDensity)}`},
+
+      // Halstead
+      { 'Length': `${chalk.blue(item.halstead.length)}`},
+      { 'Vocabulary': `${chalk.blue(item.halstead.vocabulary)}`},
+      { 'Difficulty': `${chalk.blue(item.halstead.difficulty)}`},
+      { 'Volume': `${chalk.blue(item.halstead.volume)}`},
+      { 'Effort': `${chalk.blue(item.halstead.effort)}`},
+      { 'Bugs': `${chalk.blue(item.halstead.bugs)}`},
+      { 'Time': `${chalk.blue(item.halstead.time)}`}
+    )
+
+    console.log(table.toString())
+  })
+}
+
+module.exports.html = () => {
+  // TODO
+}
+
+module.exports.json = (report, destination) => {
+  const output = JSON.stringify(report, null, 2)
+  fs.writeFile(destination, output)
+}
